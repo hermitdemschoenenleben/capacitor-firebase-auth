43e2afff5cffd46dbc2d03e3e877befd523055b6
Benjamin Wiegand <highwaychile@posteo.de>
2022-11-02 14:58:13 +0100

    apple login: don't request full name, just email

--- ios/Plugin/Handlers/AppleProviderHandler.swift
+++ ios/Plugin/Handlers/AppleProviderHandler.swift
@@ -8,17 +8,17 @@ import CryptoKit
 
 class AppleProviderHandler: NSObject, ProviderHandler  {
 
-    
-    
+
+
     var plugin: CapacitorFirebaseAuth? = nil
     var currentNonce: String?
-    
+
     func initialize(plugin: CapacitorFirebaseAuth) {
         print("Initializing Google Provider Handler")
-        
+
         self.plugin = plugin
     }
-    
+
     func signIn(call: CAPPluginCall) {
         if #available(iOS 13, *) {
             self.startSignInWithAppleFlow()
@@ -27,27 +27,27 @@ class AppleProviderHandler: NSObject, ProviderHandler  {
         }
     }
 
-    
+
     func isAuthenticated() -> Bool {
         return false
     }
-    
+
     func signOut() {
         // there is nothing to do here
         print("AppleProviderHandler.signOut called.")
     }
-    
+
     func fillResult(credential: AuthCredential?, data: PluginCallResultData) -> PluginCallResultData {
         var jsResult: PluginCallResultData = [:]
-        
+
         data.forEach { (key, value) in
             jsResult[key] = value
         }
-        
+
         let appleCredential = credential as! OAuthCredential
         jsResult["idToken"] = appleCredential.idToken
         jsResult["rawNonce"] = currentNonce
-        
+
         return jsResult
     }
 }
@@ -55,14 +55,14 @@ class AppleProviderHandler: NSObject, ProviderHandler  {
 
 @available(iOS 13.0, *)
 extension AppleProviderHandler: ASAuthorizationControllerDelegate, ASAuthorizationControllerPresentationContextProviding {
-    
+
     @available(iOS 13, *)
     func startSignInWithAppleFlow() {
       let nonce = randomNonceString()
       currentNonce = nonce
       let appleIDProvider = ASAuthorizationAppleIDProvider()
       let request = appleIDProvider.createRequest()
-      request.requestedScopes = [.fullName, .email]
+      request.requestedScopes = [.email]
       request.nonce = sha256(nonce)
 
       let authorizationController = ASAuthorizationController(authorizationRequests: [request])
@@ -70,7 +70,7 @@ extension AppleProviderHandler: ASAuthorizationControllerDelegate, ASAuthorizati
       authorizationController.presentationContextProvider = self
       authorizationController.performRequests()
     }
-    
+
     func authorizationController(controller: ASAuthorizationController, didCompleteWithAuthorization authorization: ASAuthorization) {
         if let appleIDCredential = authorization.credential as? ASAuthorizationAppleIDCredential {
             guard let nonce = currentNonce else {
@@ -88,25 +88,25 @@ extension AppleProviderHandler: ASAuthorizationControllerDelegate, ASAuthorizati
                 self.plugin!.handleError(message: "A failure occurs in Apple sign in: Unable to serialize token string from data: \(appleIDToken.debugDescription)")
                 return
             }
-            
+
             // Initialize a Firebase credential.
             let credential = OAuthProvider.credential(withProviderID: "apple.com", idToken: idTokenString, rawNonce: nonce)
-            
+
             // Sign in with Firebase.
             self.plugin?.handleAuthCredentials(credential: credential)
         }
     }
-    
+
     func authorizationController(controller: ASAuthorizationController, didCompleteWithError error: Error) {
         // Handle error.
         print("A failure occurs in Apple sign in: \(error)")
         self.plugin!.handleError(message: "A failure occurs in Apple sign in: \(error)")
     }
-    
+
     func presentationAnchor(for controller: ASAuthorizationController) -> ASPresentationAnchor {
         return (self.plugin!.bridge?.webView?.window)!
     }
-    
+
     // Adapted from https://auth0.com/docs/api-auth/tutorials/nonce#generate-a-cryptographically-random-nonce
     private func randomNonceString(length: Int = 32) -> String {
         precondition(length > 0)
@@ -114,7 +114,7 @@ extension AppleProviderHandler: ASAuthorizationControllerDelegate, ASAuthorizati
             Array("0123456789ABCDEFGHIJKLMNOPQRSTUVXYZabcdefghijklmnopqrstuvwxyz-._")
         var result = ""
         var remainingLength = length
-        
+
         while remainingLength > 0 {
             let randoms: [UInt8] = (0 ..< 16).map { _ in
                 var random: UInt8 = 0
@@ -124,22 +124,22 @@ extension AppleProviderHandler: ASAuthorizationControllerDelegate, ASAuthorizati
                 }
                 return random
             }
-            
+
             randoms.forEach { random in
                 if remainingLength == 0 {
                     return
                 }
-                
+
                 if random < charset.count {
                     result.append(charset[Int(random)])
                     remainingLength -= 1
                 }
             }
         }
-        
+
         return result
     }
-    
+
     @available(iOS 13, *)
     private func sha256(_ input: String) -> String {
         let inputData = Data(input.utf8)
@@ -147,8 +147,8 @@ extension AppleProviderHandler: ASAuthorizationControllerDelegate, ASAuthorizati
         let hashString = hashedData.compactMap {
             return String(format: "%02x", $0)
         }.joined()
-        
+
         return hashString
     }
-    
+
 }
